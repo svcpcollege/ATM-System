b89a98c9c81385dcfa6d2b5fb371dedd34ff1937
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2022-11-23 03:26:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,54 @@
 // An Automated teller Machine (ATM) program
+#include <iostream>
+using namespace std;
+
+class User
+{
+private:
+    /* data */
+protected:
+    long int user_account_number;
+    long int user_card_number;
+    int user_PIN;
+
+public:
+    User();
+    ~User();
+};
+
+User::User()
+{
+}
+
+User::~User()
+{
+}
+
+class ATM : public User
+{
+private:
+    const long int MAX_VOLUME = 1000000;
+    const long int MAX_WITHDRAW = 100000;
+    const bool ATM_Status = true;
+    long int withdraw_amount;
+    long int deposit_amount;
+    long int transfer_amount;
+
+public:
+    ATM();
+    ~ATM();
+};
+
+ATM::ATM()
+{
+}
+
+ATM::~ATM()
+{
+}
+
+int main()
+{
+    ATM atm;
+    return 0;
+}
